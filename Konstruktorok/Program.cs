3ce28fcd0f694ed83f5abd248e01a19edd6872ab
GreatAzsi <akospotfiok@gmail.com>
2026-09-16 11:05:03 +0200

    Did Task 1

--- Konstruktorok/Program.cs
+++ Konstruktorok/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dog dog = new Dog("Bodri", "Labrador", 5);
+            Dog dog2 = new Dog("Morzsi", "Beagle", 3);
         }
     }
 }
