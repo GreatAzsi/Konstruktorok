565b7da2ec22dc5473c28aef856614fa02004313
GreatAzsi <akospotfiok@gmail.com>
2026-09-16 11:18:21 +0200

    Did Task 3

---
+++ Konstruktorok/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktorok
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Points { get; set; }
+        public Player(string name, int level = 1, int points = 0)
+        {
+            Name = name;
+            Level = level;
+            Points = points;
+        }
+        public override string ToString()
+        {
+            return $"{Name} : {Level} : {Points}";
+        }
+    }
+}
--- Konstruktorok/Program.cs
+++ Konstruktorok/Program.cs
@@ -12,6 +12,10 @@
             Phone phone2 = new Phone("Apple", "iPhone 16", 350000);
             Console.WriteLine(phone);
             Console.WriteLine(phone2);
+            Player player = new Player("Steve");
+            Player player2 = new Player("Alex", 5, 1200);
+            Console.WriteLine(player);
+            Console.WriteLine(player2);
         }
     }
 }
